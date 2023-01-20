c4c0a27dfe96e5ae7f699e6a1a40f31d39d2db08
Bhargava Sai Macha <mbsai29@gmail.com>
2023-01-20 23:01:49 +0530

    add new task

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,10 +1,13 @@
 from locust import FastHttpUser, task
 
 class BSApis(FastHttpUser):
-    @task
+    @task(10)
     def basic(self):
         self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD")
         self.client.get("/launchpad/v1/whitelist/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
         self.client.get("/launchpad/v1/available/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
         self.client.get("/launchpad/v1/collection_by_name/chipfunks")
+    @task(1)
+    def basic(self):
+        self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD?force=true")
         
