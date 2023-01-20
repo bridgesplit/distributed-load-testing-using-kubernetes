9fa85e9b92c0705a8cdc026866d379637fc3ecea
Bhargava Sai Macha <mbsai29@gmail.com>
2023-01-20 22:05:24 +0530

    update endpoints

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -3,17 +3,9 @@ from locust import FastHttpUser, task
 class BSApis(FastHttpUser):
     @task
     def basic(self):
-        self.client.get("/bs_token_list")
-        self.client.get("/collection/stats")
-        self.client.get("/user/ta/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/So11111111111111111111111111111111111111112")
-        self.client.get("/user/ta/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/user/tokens/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn")
-        self.client.get("/get_all_magic_eden_prices")
-        self.client.get("/magic_eden_stats/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/fractional_pools/mint/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/single_asset_vaults/mint/AsUbiXVvVv9xAgRVvEfi441QUizgr3MaZVj5sdX11vqY")
-        self.client.get("/user/nfts/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn")
-        self.client.get("/num_staked_in_pool/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/token/lookup_table/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/rarity/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
-        self.client.get("/fractional_pools/cached_mint/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/launchpad/v1/all_collections")
+        self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD")
+        self.client.get("/launchpad/v1/whitelist/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
+        self.client.get("/launchpad/v1/available/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
+        self.client.get("/launchpad/v1/collection_by_name/chipfunks")
+        
