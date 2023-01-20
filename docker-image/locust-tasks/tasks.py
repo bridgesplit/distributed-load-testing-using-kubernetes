babe0f3688d510a7134be1fe0f3edd2767ecb600
Bhargava Sai Macha <mbsai29@gmail.com>
2023-01-20 20:47:25 +0530

    use custom endpoints

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,46 +1,19 @@
-#!/usr/bin/env python
+from locust import FastHttpUser, task
 
-# Copyright 2022 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-import uuid
-
-from datetime import datetime
-from locust import FastHttpUser, TaskSet, task
-
-
-# [START locust_test_task]
-
-class MetricsTaskSet(TaskSet):
-    _deviceid = None
-
-    def on_start(self):
-        self._deviceid = str(uuid.uuid4())
-
-    @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
-
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
-
-
-class MetricsLocust(FastHttpUser):
-    tasks = {MetricsTaskSet}
-
-# [END locust_test_task]
+class BSApis(FastHttpUser):
+    @task
+    def basic(self):
+        self.client.get("/bs_token_list")
+        self.client.get("/collection/stats")
+        self.client.get("/user/ta/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/So11111111111111111111111111111111111111112")
+        self.client.get("/user/ta/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/user/tokens/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn")
+        self.client.get("/get_all_magic_eden_prices")
+        self.client.get("/magic_eden_stats/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/fractional_pools/mint/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/single_asset_vaults/mint/AsUbiXVvVv9xAgRVvEfi441QUizgr3MaZVj5sdX11vqY")
+        self.client.get("/user/nfts/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn")
+        self.client.get("/num_staked_in_pool/4PvYpgZLarj2dbMEYPuCwaPd7aZ6fvmsZqvDswq4EjUn/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/token/lookup_table/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/rarity/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
+        self.client.get("/fractional_pools/cached_mint/4v3UTV9jibkhPfHi5amevropw6vFKVWo7BmxwQzwEwq6")
