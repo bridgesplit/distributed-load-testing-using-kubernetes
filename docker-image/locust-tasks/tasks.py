2255853935fc2639261c6817d2534c32748c7213
Luke Truitt <luke@truittluke.com>
2023-01-20 15:52:34 -0500

    users

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -15,9 +15,9 @@ class BsTaskSet(TaskSet):
         self.client.get("/launchpad/v1/whitelist/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
         # self.client.get("/user/nfts_without_meta/7bz4h6T4p39ozCnz7vpDmmiFNssyV1d1Hu3TKcDXmSXu")
         self.client.get("/launchpad/v1/collection_by_name/chipfunks")
-    @task(1)
-    def force(self):
-        self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD?force=true")
+    # @task(1)
+    # def force(self):
+    #     self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD?force=true")
 
 
 class MetricsLocust(FastHttpUser):
