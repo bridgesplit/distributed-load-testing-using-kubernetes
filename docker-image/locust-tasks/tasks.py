b2f41dd0de7a0c26f7daeea01c93c809c54648bf
Bhargava Sai Macha <mbsai29@gmail.com>
2023-01-20 22:30:49 +0530

    remove all collections

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -3,7 +3,6 @@ from locust import FastHttpUser, task
 class BSApis(FastHttpUser):
     @task
     def basic(self):
-        self.client.get("/launchpad/v1/all_collections")
         self.client.get("/launchpad/v1/stage/21bVUE7tMyqkzHcnWeLca45PFUwkwLU47578GQFKpfpD")
         self.client.get("/launchpad/v1/whitelist/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
         self.client.get("/launchpad/v1/available/EdNcP1gw4si8VUrRyP2BnaSV9H971gF9tawDhLbboZv5")
